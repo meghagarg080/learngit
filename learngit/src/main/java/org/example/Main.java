f564e135a2ec25848c540a9e6493ebb337fddce6
meghagarg080 <meghagarg080@gamil.com>
2022-09-18 14:30:40 +0530

    2nd commit

--- learngit/src/main/java/org/example/Main.java
+++ learngit/src/main/java/org/example/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args)
 
     {
-        int a = 5;
-        int b = 6;
+        int a = 12;
+        int b = 3;
 
         System.out.println(a*b);
     }
